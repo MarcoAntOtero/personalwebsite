f889324356951bed6b012051da868d9ca51fb4b2
Marco Otero <oterom935@gmail.com>
2025-06-27 15:08:55 -0600

    Added images for route, changed margins for main content, content remains and margin shrinks, got rid of old ideas

--- main.js
+++ main.js
@@ -13,72 +13,6 @@ window.addEventListener("scroll", () => {
   }
 });
 
-/*
-const outsideLinks = document.querySelector('.outside-links');
-let triggerPoint = outsideLinks.offsetTop;
-
-window.addEventListener('resize', () => {
-  triggerPoint = outsideLinks.offsetTop;
-});
-
-window.addEventListener('scroll', () => {
-  const scrollTop = window.scrollY;
-  const width = window.innerWidth;
-
-  if (width > 1350) {
-    if (scrollTop > triggerPoint - 100) {
-      outsideLinks.classList.add('fixed');
-    } else {
-      outsideLinks.classList.remove('fixed');
-    }
-  } else if (width > 850) {
-    if (scrollTop > triggerPoint - 50) {
-      outsideLinks.classList.add('fixed');
-    } else {
-      outsideLinks.classList.remove('fixed');
-    }
-  } else {
-    outsideLinks.classList.remove('fixed');
-  }
-});
-
-
-window.addEventListener('scroll', () => {
-  const scrollTop = window.scrollY;
-
-
-  if (window.innerWidth > 1350) // normal big width
-  {
-    if (scrollTop > outsideLinks.offsetTop - 100) 
-    {
-      outsideLinks.classList.add('fixed');
-    } 
-    else 
-    {
-      outsideLinks.classList.remove('fixed');
-    }
-  } 
-  else if(window.innerWidth > 850)
-  {
-    if (scrollTop > outsideLinks.offsetTop - 50) 
-      {
-        outsideLinks.classList.add('fixed');
-      } 
-      else {
-      outsideLinks.classList.remove('fixed');
-    }
-
-  }
-  else
-  {
-    // On small screens, always remove fixed
-    outsideLinks.classList.remove('fixed');
-  }
-});*/
-
-
-
-
 const sections = document.querySelectorAll('.project1-container, .project2-container,.project3-container');
 
 const observer = new IntersectionObserver(entries => {
@@ -93,7 +27,7 @@ const observer = new IntersectionObserver(entries => {
     }
   });
 }, {
-  threshold: 0.3, // Trigger when 10% of the element is visible
+  threshold: 0.37, // Trigger when 10% of the element is visible
 });
 
 sections.forEach(section => {
