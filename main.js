dc20cea7067a23b7f49bef1b0f0fa1fcafab2735
Marco Otero <oterom935@gmail.com>
2025-06-30 14:09:12 -0600

    Trying to get outside links to scroll with user

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 const outsideLinks = document.querySelector(".outside-links");
-let offsetTop = outsideLinks.offsetTop;
+/*let offsetTop = outsideLinks.offsetTop;
+
 
 window.addEventListener("resize", () => {
   offsetTop = outsideLinks.offsetTop;
 });
-
+*/
+const observer2 = new IntersectionObserver(
+  ([entry]) => {
+    if (entry) {
+      outsideLinks.classList.add("slide-in");
+    } 
+    else {
+      outsideLinks.classList.remove("slide-in");
+    }
+  },
+  {threshold: 1.0}
+);
+observer2.observe(outsideLinks);
+/*
 window.addEventListener("scroll", () => {
   if (window.scrollY > offsetTop) {
-    outsideLinks.classList.add("fixed");
+    outsideLinks.classList.add("slide-in");
   } else {
-    outsideLinks.classList.remove("fixed");
+    outsideLinks.classList.remove("slide-in");
   }
-});
+});*/
 
 const sections = document.querySelectorAll('.project1-container, .project2-container,.project3-container');
 
@@ -20,7 +34,6 @@ const observer = new IntersectionObserver(entries => {
     if (entry.isIntersecting) {
       // Element has entered the viewport
       entry.target.classList.add('is-visible');
-
     } else {
       // Element has left the viewport
       entry.target.classList.remove('is-visible');
@@ -33,7 +46,9 @@ const observer = new IntersectionObserver(entries => {
 sections.forEach(section => {
   observer.observe(section);
 });
+observer2.observe(outsideLinks);
 
+//Scroll to top btn
 document.addEventListener("DOMContentLoaded", () => {
     const scrollToTopBtn = document.getElementById("scrollToTopBtn");
     // Smoothly scroll to the top of the page
